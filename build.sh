00c09e453589ba54b7c3e29a449c2746b2c620e9
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2014-03-14 15:31:40 -0400

    updated efficiency

--- build.sh
+++ build.sh
@@ -2,31 +2,37 @@
 BASE=$(pwd)
 
 
+# Flush log
+echo "" > build.log
+
+
 # pull submodules
-git submodule init
-git submodule update --recursive
+git submodule init  >> build.log 2>&1
+git submodule update --recursive  >> build.log 2>&1
 
 
 # Get UTHASH for Smockron
-git submodule init ${BASE}/pkgs/smockron
-git submodule update --recursive ${BASE}/pkgs/smockron
+git submodule init ${BASE}/pkgs/smockron  >> build.log 2>&1
+git submodule update --recursive ${BASE}/pkgs/smockron  >> build.log 2>&1
 
 
 # Mac OS X check
 if [ $OSTYPE == "darwin13" ]; then
-    
+
+    echo "!!! Building for OS X"
     # Fix deprecated md5 features
     export FLAGS="--with-cc-opt='-Wno-deprecated-declarations'"
 
 elif [ $OSTYPE == "linux-gnu" ]; then
     
+    echo "!!! Building for Linux"
     # Check if zeromq is installed
     if [[ -n $(ldconfig -p |grep zmq.so.3) ]]; then
         yum remove zeromq zeromq-devel
-        wget http://download.zeromq.org/zeromq-3.2.2.tar.gz
-        tar zxvf zeromq-3.2.2.tar.gz && cd zeromq-3.2.2
-        ./configure
-        make && make install
+        wget http://download.zeromq.org/zeromq-3.2.2.tar.gz  >> build.log 2>&1
+        tar zxvf zeromq-3.2.2.tar.gz  >> build.log 2>&1 && cd zeromq-3.2.2  >> build.log 2>&1
+        ./configure  >> build.log 2>&1
+        make  >> build.log 2>&1 && make install >> build.log 2>&1
         cd ..
         rm -rf zeromq*
     fi
@@ -36,11 +42,19 @@ elif [ $OSTYPE == "linux-gnu" ]; then
 
 fi
 
+
 # Enter into nginx source dir
 cd src/
-patch -p1 < ../patches/001_nginx_ver.patch
+
+# Apply NGINX Patch
+echo "Patching Rocketbox..."
+patch -p1 -s < ../patches/001_nginx_ver.patch >> build.log
+
+echo "Configuring Rocketbox..."
+
+# Configure
 ./configure \
-    --sbin-path=/usr/sbin/nginx
+    --sbin-path=/usr/sbin/nginx \
     --conf-path=/etc/nginx/nginx.conf \
     --error-log-path=/var/log/nginx/error.log \
     --http-log-path=/var/log/nginx/access.log \
@@ -58,8 +72,17 @@ patch -p1 < ../patches/001_nginx_ver.patch
     --add-module=${BASE}/pkgs/headers-more-nginx-module/ \
     --add-module=${BASE}/pkgs/smockron/nginx/ \
     --add-module=${BASE}/pkgs/requestid/ \
-    ${FLAGS}
+    ${FLAGS} >> build.log 2>&1
 
 
 # Build and install
-make && sudo make install
+echo "Building Rocketbox..." 
+make >> build.log 2>&1 && echo "Installing Rocketbox..." && sudo make install >> build.log 2>&1
+
+# Test
+NGINX_TEST=$(sudo /usr/sbin/nginx -t >> build.log 2>&1)
+if [[ $? -eq 0 ]]; then
+    echo "Rocketbox installed successfully"
+else
+    echo "There was an issue installing rocketbox"
+fi
