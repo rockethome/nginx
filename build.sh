930f700b4cf41e7bc9ad6760e0be16f6570b598c
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2014-03-14 11:40:51 -0400

    removing autoindex,webdav and ssi

--- build.sh
+++ build.sh
@@ -45,13 +45,13 @@ cd src/
     --http-log-path=/var/log/nginx/access.log \
     --pid-path=/var/run/nginx.pid \
     --lock-path=/var/lock/nginx.lock \
-    --with-http_dav_module \
     --http-client-body-temp-path=/var/tmp/nginx/body \
     --http-proxy-temp-path=/var/tmp/nginx/proxy \
     --http-fastcgi-temp-path=/var/tmp/nginx/fastcgi \
-    --with-http_stub_status_module \
     --with-debug \
     --with-http_ssl_module \
+    --without-http_autoindex_module \
+    --without-http_ssi_module \
     --with-pcre \
     --with-ipv6 \
     --add-module=${BASE}/pkgs/headers-more-nginx-module/ \
