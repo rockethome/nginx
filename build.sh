954f29cc7dca0275cbf6623d98a0fe0c0090cd6f
Mike Mackintosh <mike.mackintosh@angrystatic.com>
2014-03-17 11:07:30 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 BASE=$(pwd)
-
+PLUGIN_NAME="rocketbox"
 
 # Install WGET
 sudo yum install wget
@@ -58,22 +58,27 @@ echo "Patching Rocketbox..."
 patch -p1 -s < ../patches/001_nginx_ver.patch >> build.log
 
 
+# Create nginx user
+sudo useradd -s /bin/false $PLUGIN_NAME
+
 # Configure
 echo "Configuring Rocketbox..."
 ./configure \
-    --sbin-path=/usr/sbin/nginx \
-    --conf-path=/etc/nginx/nginx.conf \
-    --error-log-path=/var/log/nginx/error.log \
-    --http-log-path=/var/log/nginx/access.log \
-    --pid-path=/var/run/nginx.pid \
-    --lock-path=/var/lock/nginx.lock \
-    --http-client-body-temp-path=/var/tmp/nginx/body \
-    --http-proxy-temp-path=/var/tmp/nginx/proxy \
-    --http-fastcgi-temp-path=/var/tmp/nginx/fastcgi \
+    --sbin-path=/usr/sbin/$PLUGIN_NAME \
+    --conf-path=/etc/rocketbox/$PLUGIN_NAME.conf \
+    --error-log-path=/var/log/$PLUGIN_NAME/error.log \
+    --http-log-path=/var/log/$PLUGIN_NAME/access.log \
+    --pid-path=/var/run/$PLUGIN_NAME.pid \
+    --lock-path=/var/lock/$PLUGIN_NAME.lock \
+    --http-client-body-temp-path=/var/tmp/$PLUGIN_NAME/body \
+    --http-proxy-temp-path=/var/tmp/$PLUGIN_NAME/proxy \
+    --http-fastcgi-temp-path=/var/tmp/$PLUGIN_NAME/fastcgi \
     --with-debug \
     --with-http_ssl_module \
     --without-http_autoindex_module \
     --without-http_ssi_module \
+    --user=$PLUGIN_NAME \
+    --group=$PLUGIN_NAME \
     --with-pcre \
     --with-ipv6 \
     --add-module=${BASE}/pkgs/headers-more-nginx-module/ \
@@ -82,13 +87,23 @@ echo "Configuring Rocketbox..."
     ${FLAGS} >> build.log 2>&1
 
 
-# Build and install
+# Building
 echo "Building Rocketbox..." 
-make >> build.log 2>&1 && echo "Installing Rocketbox..." && sudo make install >> build.log 2>&1
+make >> build.log 2>&1
+
+
+# Installing nginx
+echo "Installing Rocketbox..."
+sudo make install >> build.log 2>&1
+
+
+# Create nginx dirs
+sudo mkdir -p /var/tmp/$PLUGIN_NAME/{body,proxy,fastcgy}
+
 
 
 # Test
-NGINX_TEST=$(sudo /usr/sbin/nginx -t >> build.log 2>&1)
+NGINX_TEST=$(sudo /usr/sbin/$PLUGIN_NAME -t >> build.log 2>&1)
 if [[ $? -eq 0 ]]; then
     echo "Rocketbox installed successfully"
 else
